3e3c4cc6b8621b9cf7b4dd802349b2788a81ee87
sevicd <43240891+sevicd@users.noreply.github.com>
2018-10-11 17:56:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://www.plusplaner.com");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("Username")).SendKeys("user21");
+            driver.FindElement(By.Id("password")).SendKeys("Pass21" + Keys.Enter);
+        }
+    }
+}
